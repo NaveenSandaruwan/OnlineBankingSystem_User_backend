355e5b3e5f789313fbbfdd94f4b2a19df0765163
NaveenSandaruwan <157776142+NaveenSandaruwan@users.noreply.github.com>
2024-12-18 21:57:23 +0530

    Refactor UserAccountDTO and Account models; update authentication service to include user details

--- src/main/java/com/customer_backend/dto/UserAccountDTO.java
+++ src/main/java/com/customer_backend/dto/UserAccountDTO.java
@@ -1,10 +1,21 @@
 package com.customer_backend.dto;
 
 public class UserAccountDTO {
+    private Long userId;
     private String firstName;
     private String lastName;
+    private String phone;
+    private String address;
 
     // Getters and Setters
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -19,5 +30,21 @@ public class UserAccountDTO {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
